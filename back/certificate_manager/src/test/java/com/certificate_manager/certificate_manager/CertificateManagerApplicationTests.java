148e50394e7c1eec2861ed63c8e63557275bf3c0
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-03-26 18:48:10 +0200

    created spring  boot project

---
+++ back/certificate_manager/src/test/java/com/certificate_manager/certificate_manager/CertificateManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certificate_manager.certificate_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
