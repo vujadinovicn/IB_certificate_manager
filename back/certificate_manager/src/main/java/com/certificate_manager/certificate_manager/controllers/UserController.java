13cfba0abc9ac7c5e026dc52993ab1387f6ccd3b
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2023-03-30 20:29:06 +0200

    implemented flow from controller over service to repository and db for registration

--- back/certificate_manager/src/main/java/com/certificate_manager/certificate_manager/controllers/UserController.java
+++ back/certificate_manager/src/main/java/com/certificate_manager/certificate_manager/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.certificate_manager.certificate_manager.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.certificate_manager.certificate_manager.dtos.UserDTO;
+import com.certificate_manager.certificate_manager.exceptions.UserAlreadyExistsException;
+import com.certificate_manager.certificate_manager.services.interfaces.IUserService;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 	
+	@Autowired
+	private IUserService userService;
+	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> insertPassenger(@RequestBody UserDTO dto) {
-		System.out.println(dto);
-		return new ResponseEntity<String>("eee", HttpStatus.OK);
+	public ResponseEntity<?> insertPassenger(@RequestBody UserDTO userDTO) {
+		try {
+			this.userService.register(userDTO);
+			return new ResponseEntity<String>("You have successfully registered!", HttpStatus.OK);
+		} catch (UserAlreadyExistsException e) {
+			return new ResponseEntity<String>("User with this email already exists!", HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
