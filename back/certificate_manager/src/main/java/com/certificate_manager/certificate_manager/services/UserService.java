6104c538754892a7f83cc5d1055f8f27d7fbd1b4
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2023-03-30 19:55:38 +0200

    created UserService which inherits IUserService interface

---
+++ back/certificate_manager/src/main/java/com/certificate_manager/certificate_manager/services/UserService.java
@@ -0,0 +1,16 @@
+package com.certificate_manager.certificate_manager.services;
+
+import org.springframework.stereotype.Service;
+
+import com.certificate_manager.certificate_manager.dtos.UserDTO;
+import com.certificate_manager.certificate_manager.services.interfaces.IUserService;
+
+@Service
+public class UserService implements IUserService{
+
+	@Override
+	public void register(UserDTO userDTO) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ back/certificate_manager/src/main/java/com/certificate_manager/certificate_manager/services/interfaces/IUserService.java
@@ -0,0 +1,9 @@
+package com.certificate_manager.certificate_manager.services.interfaces;
+
+import com.certificate_manager.certificate_manager.dtos.UserDTO;
+
+public interface IUserService {
+	
+	public void register(UserDTO userDTO);
+
+}
