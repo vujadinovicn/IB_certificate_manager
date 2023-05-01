1948850be1e4d47dbf29f4adaee841c46e9d915c
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2023-05-01 02:46:27 +0200

    implemented navbar component

---
+++ front/certificate_manager_front/src/app/navbar/navbar.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit{
+
+  role: string = "";
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    this.handleSmallScreens();
+  }
+
+  handleSmallScreens(): void {
+    (<HTMLButtonElement>document.querySelector('.navbar-toggler'))
+      .addEventListener('click', () => {
+      let navbarMenu = <HTMLDivElement>document.querySelector('.navbar-menu')
+  
+      if (navbarMenu.style.display === 'flex') {
+        navbarMenu.style.display = 'none'
+        return 
+      }
+  
+      navbarMenu.style.display = 'flex'
+    })
+  }
+
+  logout(){
+
+  }
+
+  openHome(role: string){
+    
+  }
+}
