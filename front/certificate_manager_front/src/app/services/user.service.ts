af350205fe42b55b9ace76a2e4c3a9b9c0b479c8
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-05-02 19:09:49 +0200

    created user service with registration post method

---
+++ front/certificate_manager_front/src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(user: UserDTO): Observable<any> {
+    const options: any = {
+      responseType: 'json',
+    };
+    return this.http.post<any>(environment.apiHost + "/user", user, options);
+  }
+}
+
+export interface UserDTO {
+  name: string;
+  surname: string;
+  profilePicture: string | null;
+  telephoneNumber: string;
+  email: string;
+  password: string;
+}
