09fd69575249597a23b34bf4a073282c7ac13ed1
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-05-02 23:31:48 +0200

    added method in user service for verifying registration

--- front/certificate_manager_front/src/app/services/user.service.ts
+++ front/certificate_manager_front/src/app/services/user.service.ts
@@ -16,6 +16,10 @@ export class UserService {
     };
     return this.http.post<any>(environment.apiHost + "/user", user, options);
   }
+
+  verify(code: String): Observable<any> {
+    return this.http.get<any>(environment.apiHost + '/user/' + code);
+  }
 }
 
 export interface UserDTO {
