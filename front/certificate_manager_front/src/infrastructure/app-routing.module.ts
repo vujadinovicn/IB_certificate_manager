46f1a6df8979f90e0e7d793127b177563f1c5f86
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-05-02 19:55:19 +0200

    fixed responsive design in mailVerification component

--- front/certificate_manager_front/src/infrastructure/app-routing.module.ts
+++ front/certificate_manager_front/src/infrastructure/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from 'src/app/login/login.component';
+import { MailVerificationComponent } from 'src/app/mail-verification/mail-verification.component';
+import { RegisterComponent } from 'src/app/register/register.component';
 
 const routes: Routes = [
-  {path: "login", component: LoginComponent}
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "verification/mail", component: MailVerificationComponent}
 ];
 
 @NgModule({
