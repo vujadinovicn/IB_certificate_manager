34317edf89f1c6865c68e0087d63439b0f478c08
tince250 <t.s.mihajlovic25@gmail.com>
2023-05-02 11:27:41 +0200

    added auth service - login, logout and jwt reading functions

--- front/certificate_manager_front/src/app/login/login.component.ts
+++ front/certificate_manager_front/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,21 +9,42 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
-  ngOnInit(): void {
-    // throw new Error('Method not implemented.');
-  }
-
-  constructor(){
-    
-  }
   isVisible : boolean = false;
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required])
-  })
+  });
+
+  constructor(private authService: AuthService, public snackBar: MatSnackBar){
+    
+  }
+  
+  ngOnInit(): void {
+
+  }
 
   login(){
+    const credentials = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    };
 
+    if (this.loginForm.valid) {
+      this.authService.login(credentials).subscribe({
+        next: (result) => {
+          localStorage.setItem('user', JSON.stringify(result.accessToken));
+          localStorage.setItem('refreshToken', JSON.stringify(result.refreshToken));
+          this.authService.setUser();
+          console.log("USPEH")
+        },
+        error: (error) => {
+          console.log(error)
+          this.snackBar.open("Bad credentials. Please try again!", "", {
+            duration: 2000,
+         });
+        },
+      });
+    }
   }
 }
