35d0f09b1f0226dc73b03d608a8b5611d0c48e9e
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2023-05-07 20:57:17 +0200

    created scheduled controller. Added schedule annotations

---
+++ back/certificate_manager/src/main/java/com/certificate_manager/certificate_manager/controllers/ScheduledController.java
@@ -0,0 +1,22 @@
+package com.certificate_manager.certificate_manager.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+import com.certificate_manager.certificate_manager.services.interfaces.IScheduledService;
+
+import jakarta.transaction.Transactional;
+
+@Controller
+public class ScheduledController {
+	
+	@Autowired 
+	private IScheduledService scheduledService; 
+
+	@Transactional
+	@Scheduled(initialDelayString = "${initialdelay}", fixedRateString = "${fixedratearrival}")
+	public void test() {
+		
+	} 
+} 
